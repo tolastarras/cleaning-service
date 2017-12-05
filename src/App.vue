23886ca6cb216cea8d08b552aeb972e6f77e34ab
Rafael <rafael@nerd.com>
2017-12-04 22:54:20 -0500

    initial carousel

--- src/App.vue
+++ src/App.vue
@@ -47,21 +47,9 @@
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-        </v-slide-y-transition>
-      </v-container>
+      <main>
+        <router-view></router-view>
+      </main>
     </v-content>
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
