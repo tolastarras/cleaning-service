8c29c811796a3d16569a0e6a141cb5dff2517087
Rafael <rafael@nerd.com>
2017-12-05 11:19:48 -0500

    limited max page width and center carousel in home page

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,61 @@
 <template>
   <v-app style="background:url(/static/bg.png)">
-    <v-navigation-drawer
-      temporary
-      absolute
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-list-tile 
-          value="true"
-          v-for="(item, i) in menuItems"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <div class="toolbar_extension">
+    <v-container style="max-width:1000px;">
+      <v-navigation-drawer
+        temporary
+        absolute
+        v-model="drawer"
+        app
+      >
+        <v-list>
+          <v-list-tile 
+            value="true"
+            v-for="(item, i) in menuItems"
+            :key="i"
+          >
+            <v-list-tile-action>
+              <v-icon v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <div class="toolbar_extension">
+        <v-content>
+          <v-layout xs6 col>
+            <v-flex xs12></v-flex>
+          </v-layout>
+          
+          <div class="text-xs-center">
+            <v-btn outline color="red darken-3">Call us today {{phone}}</v-btn>
+          </div>
+  
+          <v-layout xs6 col>
+            <v-flex xs12>social media</v-flex>
+          </v-layout>
+        </v-content>
+      </div>
+      <v-toolbar flat>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only" color="primary">
+          <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.link">
+            {{item.title}}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
       <v-content>
-        <v-layout xs6 col>
-          <v-flex xs12></v-flex>
-        </v-layout>
-        
-        <div class="text-xs-center">
-          <v-btn outline color="red darken-3">Call us today {{phone}}</v-btn>
-        </div>
-
-        <v-layout xs6 col>
-          <v-flex xs12>social media</v-flex>
-        </v-layout>
+        <main>
+          <router-view></router-view>
+        </main>
       </v-content>
-    </div>
-    <v-toolbar flat>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only" color="primary">
-        <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.link">
-          {{item.title}}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-content>
-      <main>
-        <router-view></router-view>
-      </main>
-    </v-content>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
+      <v-footer :fixed="fixed" app>
+        <span>&copy; 2017</span>
+      </v-footer>
+    </v-container>
   </v-app>
 </template>
 
