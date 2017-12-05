ea1ca4fd668af9efeb67748d51a4d5f7344d7d35
Rafael <rafael@nerd.com>
2017-12-05 13:09:26 -0500

    displayed three cards under carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,6 +9,42 @@
         reverseTransition="fade"
       ></v-carousel-item>
     </v-carousel>
+
+    <v-layout column>
+      <v-flex xs12>
+        <v-container fluid grid-list-md>
+          <v-layout row wrap>
+            <v-flex
+              v-bind="{ [`xs${card.flex}`]: true }"
+              v-for="card in cards"
+              :key="card.title"
+            >
+            <h1 align-end>{{card.title}}</h1>
+
+              <v-card>
+                <v-card-media
+                  :src="card.src"
+                  height="200px"
+                >
+                </v-card-media>
+                <v-card-actions class="white">
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>favorite</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>bookmark</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>share</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -20,12 +56,13 @@ export default {
       items: [
         {src: '/static/carousel/kitchen.jpg'},
         {src: '/static/carousel/kitchen.webp'},
-        {src: '/static/carousel/bathroom.webp'}
+        {src: '/static/carousel/bathroom.webp'},
+        {src: '/static/carousel/bedroom.webp'}
       ],
       cards: [
-        { title: 'Pre-fab homes', src: '/static/carousel/kitchen.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: '/static/carousel/kitchen.jpg', flex: 6 },
-        { title: 'Best airlines', src: '/static/carousel/kitchen.jpg', flex: 6 }
+        { title: 'Quality Cleaning', src: '/static/card/sink.webp', flex: 4 },
+        { title: 'Professional Staff', src: '/static/card/mirror.webp', flex: 4 },
+        { title: 'Affordable Rates', src: '/static/card/save.webp', flex: 4 }
       ]
     }
   }
