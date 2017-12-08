fc19c6630c65eb285b0872a5875eb9aafff95e45
Rafael <rafael@nerd.com>
2017-12-08 12:39:14 -0500

    made style changes to services page

--- src/components/pages/Service.vue
+++ src/components/pages/Service.vue
@@ -1,23 +1,25 @@
 <template lang="html">
   <v-container>
+    <h1 class="display-1 pb-4 blue--text">Services</h1>
     <v-layout row wrap>
-      <v-flex xs12 sm6 md4 pr-2 pb-2 v-for="card in cards" :key="card.title">
-        <v-card>
-          <v-card-media
-            class="white--text"
-            height="300px"
-            :src="card.src"
-          >
-          </v-card-media>
-          <v-card-title>
-            <div class="text-xs-center">
-              <span class="headline grey--text">{{card.title}}</span><br>
-              <p class="pa-4">1hr | $20.00</p>
-              <p>Describe your service here. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.</p>
+      <v-flex xs12 sm6 md4 pr-4 pb-4 v-for="card in cards" :key="card.title">
+        <v-card class="elevation-1" style="height: 420px" @mouseover="toggle(card)" @mouseout="toggle(card)">
+          <transition name="slide">
+            <div>
+              <v-card-media height="200" v-show="card.show" :src="card.src"></v-card-media>
+              <v-card-title>
+                <div class="text-xs-center" style="width: 100%">
+                  <div class="headline pt-4">{{card.title}}</div>
+                  <p class="pt-4">1hr | $20.00</p>
+                  <v-card-text v-show="!card.show" style="height: 200px;">
+                    Describe your service here. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.
+                  </v-card-text>
+                </div>
+              </v-card-title>
             </div>
-          </v-card-title>
+          </transition>
           <v-card-actions>
-            <v-btn dark color="blue">Book Now</v-btn>
+            <v-btn dark color="blue" style="margin: 0 auto">Book Now</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -29,16 +31,31 @@
 export default {
   data: () => ({
     cards: [
-      { title: 'House Cleaning', src: '/static/pages/house-cleaning.jpg' },
-      { title: 'Carpet Cleaning', src: '/static/pages/carpet-cleaning.jpg' },
-      { title: 'After Party Cleaning', src: '/static/pages/after-party-cleaning.jpg' },
-      { title: 'Commercial Cleaning', src: '/static/pages/bedroom-cleaning.jpg' },
-      { title: 'Move In/Out Cleaning', src: '/static/pages/move-in-move-out-cleaning.jpg' },
-      { title: 'Upholstery Cleaning', src: '/static/pages/house-cleaning.jpg' }
+      { id: 1, title: 'House Cleaning', src: '/static/pages/house-cleaning.jpg', show: true },
+      { id: 2, title: 'Carpet Cleaning', src: '/static/pages/carpet-cleaning.jpg', show: true },
+      { id: 3, title: 'After Party Cleaning', src: '/static/pages/after-party-cleaning.jpg', show: true },
+      { id: 4, title: 'Commercial Cleaning', src: '/static/pages/bedroom-cleaning.jpg', show: true },
+      { id: 5, title: 'Move In/Out Cleaning', src: '/static/pages/move-in-move-out-cleaning.jpg', show: true },
+      { id: 6, title: 'Upholstery Cleaning', src: '/static/pages/upholstery-cleaning.jpg', show: true }
     ]
-  })
+  }),
+  methods: {
+    toggle (card) {
+      card.show = !card.show
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style scoped lang="css">
+.display-1 {
+  font-weight: bold;
+}
+/*.slide-enter-active, .slide-leave-active {
+  transition: all .5s;
+  overflow-y: hidden;
+}
+.slide-enter, .slide-leave-to {
+  transform: translateY(300px);
+}*/
 </style>
