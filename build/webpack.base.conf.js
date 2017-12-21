fda696e96473eaf5c193b40d91629fbc13ad7f67
Rafael <rafael@nerd.com>
2017-12-21 17:50:38 -0500

    limit images to 5k

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -53,8 +53,8 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          limit: 5000,
+          name: utils.assetsPath('img/[hash:7].[ext]')
         }
       },
       {
@@ -69,7 +69,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 5000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
