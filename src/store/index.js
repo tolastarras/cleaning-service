b9e23b8261a89792cfa823b2fd16e49ed71b707f
Rafael <tolastarras@gmail.com>
2019-02-25 10:04:13 -0500

    refactor vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -12,28 +12,19 @@ export const store = new Vuex.Store({
     documentWidth: window.innerWidth
   },
   mutations: {
-    SET_BUSINESS_INFO (state, payload) {
-      state.business = payload.business
+    SET_DATA (state, { business, frequencies, services, parallax }) {
+      state.business = business
+      state.services = services
+      state.frequencies = frequencies
+      state.parallax = parallax
     },
     SET_DOCUMENT_WIDTH (state, width) {
       state.documentWidth = width
-    },
-    SET_SERVICES (state, services) {
-      state.services = services
-    },
-    SET_FREQUENCIES (state, frequencies) {
-      state.frequencies = frequencies
-    },
-    SET_PARALLAX (state, payload) {
-      state.parallax = payload
     }
   },
   actions: {
     init ({ commit }, payload) {
-      commit('SET_BUSINESS_INFO', payload)
-      commit('SET_SERVICES', payload.services)
-      commit('SET_PARALLAX', payload.parallaxItems)
-      commit('SET_FREQUENCIES', payload.frequencies)
+      commit('SET_DATA', payload)
     },
     changeDocumentWidth ({ commit }, width) {
       commit('SET_DOCUMENT_WIDTH', width)
