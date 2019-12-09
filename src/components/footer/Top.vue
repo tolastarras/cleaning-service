27ba814459ee3da9fe3a0bceb1548f25f76d643a
Rafael Sobrino <rafael.sobrino@codelitt.com>
2019-12-09 00:02:30 -0500

    Replace the footer header styles with vuetify classes

--- src/components/footer/Top.vue
+++ src/components/footer/Top.vue
@@ -3,17 +3,17 @@
     <v-container class="top-footer">
       <v-layout justify-space-between row>
         <v-flex v-show="!isAboutPage" class="footer-block font-weight-thin" xs12>
-          <h2>About Us</h2>
+          <h2 class="amber--text text--accent-2">About Us</h2>
           <div v-text="business.about.text"></div>
           <router-link to="about">(read more ...)</router-link>
         </v-flex>
         <v-flex v-show="!isServicesPage && !isHomePage" class="footer-block font-weight-thin" xs12 sm4>
-          <h2>What we do</h2>
+          <h2 class="amber--text text--accent-2">What we do</h2>
           <div v-text="business.service.text"></div>
           <router-link to="services">(read more ...)</router-link>
         </v-flex>
         <v-flex v-show="!isServicesPage && !isHomePage" class="footer-block  font-weight-thin" xs12 lg4>
-          <h2>Our Services</h2>
+          <h2 class="amber--text text--accent-2">Our Services</h2>
           <v-layout row wrap>
             <v-flex text-uppercase class="service-types" xs-6 v-for="service in services" :key="service.id">
               <router-link to="services">{{ service.title }}</router-link>
@@ -21,7 +21,7 @@
           </v-layout>
         </v-flex>
         <v-flex v-show="!isContactPage && !isAboutPage" class="footer-block font-weight-thin" xs12 lg4>
-          <h2>Contact Us</h2>
+          <h2 class="amber--text text--accent-2">Contact Us</h2>
           <v-list class="contact-info-list">
             <v-list-tile :class="contact.icon" v-for="(contact, i) in contactMethods" :key="i" target="_blank" :href="contact.link">
               <v-list-tile-avatar>
@@ -34,7 +34,7 @@
           </v-list>
         </v-flex>
         <v-flex v-show="!isContactPage" class="footer-block font-weight-thin" xs12 lg4>
-          <h2 class="blue--text">Hours</h2>
+          <h2 class="amber--text text--accent-2">Hours</h2>
           <div v-html="business.hours.title"></div>
         </v-flex>
       </v-layout>
