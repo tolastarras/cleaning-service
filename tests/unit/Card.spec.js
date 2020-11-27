1e936dfed064b9bd9c0b245f99e6c852acfbef32
Rafael Sobrino <rafael@acst.com>
2020-11-26 23:26:48 -0500

    Add workflows and fix dependencies vulnerabilities

---
+++ tests/unit/Card.spec.js
@@ -0,0 +1,41 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+
+import Card from '@/components/Card';
+import helper from '@/mixins/helper'
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+Vue.use(Vuetify);
+
+describe('Card', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const store = new Vuex.Store();
+
+    wrapper = shallowMount(Card, {
+      localVue,
+      store,
+      mixins: [helper],
+      propsData: {
+        card: {
+          title: 'Test',
+          image: 'sink.jpg',
+        },
+      },
+    });
+  });
+
+  it('is instantiated', () => {
+    expect(wrapper.vm).toBeTruthy();
+  });
+
+  it('renders without breaking', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
