88b5ffb296690cfb3f9064bac8f7662faeda2122
Rafael Sobrino <rafael.sobrino@codelitt.com>
2021-04-30 18:11:01 -0400

    Optimize the card images in the home page

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,17 +3,19 @@
     <v-layout row wrap>
       <v-flex xs-12>
         <v-card flat>
-          <v-img :src="require(`@/assets/card/${card.image}`)">
-            <v-layout class="media-content"></v-layout>
+          <v-img :lazy-src="image">
+            <v-layout class="media-content" />
           </v-img>
           <v-card-text>
-            <h2 class="text-xs-center" v-html="title"></h2>
+            <h2 class="text-xs-center" v-html="title" />
             <div class="card-content">
               {{ card.content }}
             </div>
           </v-card-text>
           <v-card-actions class="justify-center">
-            <v-btn dark outline color="blue" @click="readMore">Read More</v-btn>
+            <v-btn dark outline color="blue" @click="readMore">
+              Read More
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -23,10 +25,18 @@
 
 <script>
 export default {
-  props: ['card', 'index', 'total'],
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  },
   computed: {
     title () {
       return this.$root.formatTitle(this.card.title)
+    },
+    image () {
+      return require(`@/assets/card/${this.card.image}`)
     }
   },
   methods: {
